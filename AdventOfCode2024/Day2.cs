3c0328fc824eb1af0271ae88912f63b65f06d655
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-12-07 17:28:18 +0100

    day2 refactored

--- AdventOfCode2024/Day2.cs
+++ AdventOfCode2024/Day2.cs
@@ -7,18 +7,10 @@ public class Day2
     public void Part1(string filename, int expectedResult)
     {
         var reportLines = File.ReadAllLines(filename);
-        var reports = reportLines.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToArray();
+        var reports = ParseReports(reportLines);
 
-        int validReports = 0;
-        foreach (var report in reports)
-        {
-            var diff = report.Zip(report.Skip(1), (x, y) => x - y).ToArray();
-            if (diff.All(d => d is >= 1 and <= 3) || diff.All(d => d is >= -3 and <= -1))
-            {
-                validReports++;
-            }
-        }
-        
+        int validReports = reports.Count(IsValidReport);
+
         Assert.That(validReports, Is.EqualTo(expectedResult));
     }
 
@@ -27,24 +19,30 @@ public class Day2
     public void Part2(string filename, int expectedResult)
     {
         var reportLines = File.ReadAllLines(filename);
-        var reports = reportLines.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToArray();
-        var dampenedReports = reports.Select(report =>
+        var reports = ParseReports(reportLines);
+        var dampenedReportGroups = reports.Select(report => report.Select((_, i) =>
         {
-            return report.Select((level, i) => report.Take(i).Concat(report.Skip(i + 1).ToArray())).ToArray();
-        });
+            // Using LINQ:
+            // return report.Take(i).Concat(report.Skip(i + 1).ToArray());
+                
+            // Using Collection spread and range operators:
+            return (int[])[..report[..i], ..report[(i + 1)..]];
+        }).ToArray());
 
-        int validReports = 0;
-        foreach (var reportGroup in dampenedReports)
-        {
-            validReports += reportGroup.Any(report =>
-            {
-                var diff = report.Zip(report.Skip(1), (x, y) => x - y).ToArray();
-                return diff.All(d => d is >= 1 and <= 3) || diff.All(d => d is >= -3 and <= -1);
-            })
-                ? 1
-                : 0;
-        }
-        
+        var validReports = dampenedReportGroups.Sum(reportGroup => reportGroup.Any(IsValidReport) ? 1 : 0);
+
         Assert.That(validReports, Is.EqualTo(expectedResult));
+    }
+
+    static bool IsValidReport(int[] report)
+    {
+        var diff = report.Zip(report.Skip(1), (x, y) => x - y).ToArray();
+        return diff.All(d => d is >= 1 and <= 3) || diff.All(d => d is >= -3 and <= -1);
+    }
+
+    static int[][] ParseReports(string[] reportLines)
+    {
+        var reports = reportLines.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToArray();
+        return reports;
     }
 }
