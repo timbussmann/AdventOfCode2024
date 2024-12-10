e40740bf377673858dfd7f015f2dad6d8f11cba8
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-12-10 23:07:27 +0100

    day3 - part 2 version 1

--- AdventOfCode2024/Day3.cs
+++ AdventOfCode2024/Day3.cs
@@ -10,8 +10,23 @@ public partial class Day3
     {
         var input = File.ReadAllText(filename);
         
-        var matches = InputParserRegex().Matches(input);
-        var result = matches
+        var instructions = MultiplyInstructionRegex().Matches(input);
+        var result = instructions
+            .Select(match => int.Parse(match.Groups["X"].Value) * int.Parse(match.Groups["Y"].Value))
+            .Sum();
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+    
+    [TestCase("Day3.Test2.txt", 48)]
+    [TestCase("Day3.Input.txt", 53783319)]
+    public void Part2(string filename, long expectedResult)
+    {
+        var input = File.ReadAllText(filename);
+
+        var activeInputBlockMatches = ActiveInstructionsBlocksRegex().Matches("do()" + input + "don't()");
+        var instructionMatches = activeInputBlockMatches.SelectMany(match => MultiplyInstructionRegex().Matches(match.Value));
+        var result = instructionMatches
             .Select(match => int.Parse(match.Groups["X"].Value) * int.Parse(match.Groups["Y"].Value))
             .Sum();
 
@@ -19,5 +34,9 @@ public partial class Day3
     }
 
     [GeneratedRegex(@"mul\((?<X>\d{1,3}),(?<Y>\d{1,3})\)")]
-    private static partial Regex InputParserRegex();
+    private static partial Regex MultiplyInstructionRegex();
+    
+    // lazy matching between do() and don't() as well as RegexOptions.Singleline are crucial!
+    [GeneratedRegex(@"do\(\).*?don't\(\)", RegexOptions.Singleline)]
+    private static partial Regex ActiveInstructionsBlocksRegex();
 }
