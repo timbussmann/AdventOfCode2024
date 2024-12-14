bb4ee1259228c5db6c206fdee591a6c5dcc792fb
Tim Bussmann <timbussmann@users.noreply.github.com>
2024-12-14 18:23:18 +0100

    day 5 - part 2 v1

--- AdventOfCode2024/Day5.cs
+++ AdventOfCode2024/Day5.cs
@@ -20,6 +20,26 @@ public class Day5
         
         Assert.That(result, Is.EqualTo(expectedResult));
     }
+    
+    [TestCase("Day5.Test.txt",  123)]
+    [TestCase("Day5.Input.txt",  3062)]
+    public void Part2(string filename, int expectedResult)
+    {
+        var lines = File.ReadAllLines(filename);
+        var rules = lines.TakeWhile(l => l != string.Empty).Select(line =>
+        {
+            var segments = line.Split('|');
+            return (segments[0], segments[1]);
+        }).ToList();
+        var comparer = Comparer<string>.Create((s1, s2) => rules.Contains((s1, s2)) ? -1 : 1);
+        var updates = lines.SkipWhile(l => l != string.Empty).Skip(1)
+            .Select(line => line.Split(',')).ToArray();
+
+        var reOrdered = updates.Where(u => !u.SequenceEqual(u.Order(comparer))).Select(u => u.Order(comparer).ToArray());
+        var result = reOrdered.Sum(line => int.Parse(line[line.Length/2]));
+        
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
 
     static bool IsInOrder(string[] update, ILookup<string, string> rules)
     {
