112fc48ef47579207b0fc86a8933d8fd0d7a9bbe
Tim Bussmann <tim.bussmann@digitecgalaxus.ch>
2024-12-22 16:27:19 +0100

    day7 part1 v1

---
+++ AdventOfCode2024/Day7.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2024;
+
+public class Day7
+{
+    [TestCase("Day7.Test.txt", 3749)]
+    [TestCase("Day7.Input.txt", 5702958180383)]
+    public void Part1(string filename, long expectedResult)
+    {
+        var lines = File.ReadAllLines(filename);
+        var equations = lines.Select(line =>
+        {
+            var lineParts = line.Split(":");
+            var operationElements = lineParts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return (equationResult: long.Parse(lineParts[0]),
+                equationElements: operationElements.Select(long.Parse).ToArray());
+        });
+
+        var totalResult = 
+            (from equation in equations 
+                let allResults = ComputeAllOperations(equation.equationElements[0], equation.equationElements[1..], equation.equationResult) 
+                where allResults.Any(r => r == equation.equationResult) 
+                select equation.equationResult).Sum();
+
+        Assert.That(totalResult, Is.EqualTo(expectedResult));
+    }
+
+    private static long[] ComputeAllOperations(long input, long[] nextElements, long expectedResult)
+    {
+        if (nextElements.Length == 0)
+        {
+            return [input];
+        }
+
+        List<long> results = [];
+        var additionResult = input + nextElements[0];
+        if (additionResult <= expectedResult)
+        {
+            results.AddRange(ComputeAllOperations(additionResult, nextElements[1..], expectedResult));
+        }
+        var multiplicationResult = input * nextElements[0];
+        if (multiplicationResult <= expectedResult)
+        {
+            results.AddRange(ComputeAllOperations(multiplicationResult, nextElements[1..], expectedResult));
+        }
+
+        return results.ToArray();
+    }
+}
