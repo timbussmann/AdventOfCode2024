aa33a05243646bd04df3f7e617c81ee27d9a0df1
Tim Bussmann <timbussmann@users.noreply.github.com>
2025-01-01 21:58:14 +0100

    small part2 refactoring

--- AdventOfCode2024/Day9.cs
+++ AdventOfCode2024/Day9.cs
@@ -20,8 +20,11 @@ public class Day9
             diskContent.AddRange(Enumerable.Repeat(-1, int.Parse(chunk[1].ToString())));
         }
 
-        var freeBlocks = new Stack<int>(diskContent.Select((value, index) => (value: value, index: index))
-            .Where(tuple => tuple.value == -1).Select(tuple => tuple.index).Reverse());
+        var freeBlocks = new Stack<int>(diskContent
+            .Select((value, index) => (value: value, index: index))
+            .Where(tuple => tuple.value == -1)
+            .Select(tuple => tuple.index)
+            .Reverse());
         for (int i = diskContent.Count - 1; i >= 0; i--)
         {
             if (diskContent[i] != -1)
@@ -37,8 +40,6 @@ public class Day9
             }
         }
 
-        TestContext.Out.WriteLine(string.Concat(diskContent));
-        
         long result = diskContent.TakeWhile(x => x != -1).Select((value, index) => (long)value * index).Sum();
         
         Assert.That(result, Is.EqualTo(expectedResult));
@@ -48,72 +49,75 @@ public class Day9
     [TestCase("Day9.Input.txt", 6415163624282)]
     public void Part2(string filename, long expectedResult)
     {
-        var input = File.ReadAllText(filename);
-        if (input.Length % 2 != 0)
-        {
-            input += "0";
-        }
-        var chunked = input.Chunk(2);
-        LinkedList<Range> diskContent = new LinkedList<Range>();
-        int blockID = 0;
-        int index = 0;
-        foreach (var chunk in chunked)
-        {
-            var fileLenth = int.Parse(chunk[0].ToString());
-            diskContent.AddLast(new Range(index, fileLenth, blockID));
-            blockID++;
-            index += fileLenth;
-            var freeBlockLength = int.Parse(chunk[1].ToString());
-            diskContent.AddLast(new Range(index, freeBlockLength, -1));
-            index += freeBlockLength;
-        }
+        var diskSegments = ParseDiskSegments(filename);
 
-        var currentNode = diskContent.Last;
-        while (currentNode != null)
+        var currentBlock = diskSegments.Last!;
+        while (currentBlock != null)
         {
-            if (currentNode.Value.value == -1)
+            if (currentBlock.Value.IdNumber != -1)
             {
-                //free space, skip
-                currentNode = currentNode.Previous;
+                currentBlock = RearrangeBlock(currentBlock);
             }
-            else
-            {
-                LinkedListNode<Range>? potentialBlock = diskContent.First;
-                bool moved = false;
-                while (potentialBlock != null)
-                {
-                    if (potentialBlock.Value.value == -1 && potentialBlock.Value.length >= currentNode.Value.length && potentialBlock.Value.startIndex < currentNode.Value.startIndex)
-                    {
-                        // free space
-                        diskContent.AddBefore(potentialBlock, new Range(potentialBlock.Value.startIndex, currentNode.Value.length, currentNode.Value.value));
-                        diskContent.AddBefore(potentialBlock, new Range(potentialBlock.Value.startIndex + currentNode.Value.length, potentialBlock.Value.length - currentNode.Value.length, -1));
-                        diskContent.Remove(potentialBlock);
-                        //TODO: probably have to combine separate -1 sections again
-                        diskContent.AddAfter(currentNode, currentNode.Value with { value = -1 });
-                        var next = currentNode.Previous;
-                        diskContent.Remove(currentNode);
-                        currentNode = next;
-                        moved = true;
-                        break;
-                    }
 
-                    potentialBlock = potentialBlock.Next;
-                }
-
-                if (!moved)
-                {
-                    currentNode = currentNode.Previous;
-                }
-
-            }
+            currentBlock = currentBlock.Previous;
         }
 
-        var flattenedContent = diskContent.SelectMany(range => Enumerable.Repeat(range.value, range.length)).ToList();
-        TestContext.Out.WriteLine(string.Concat(flattenedContent.Select(x => x.ToString())).Replace("-1", "."));
-        long result = flattenedContent.Select((value, index) => (long)value * index).Where(x => x > 0).Sum();
+        var diskLayout = diskSegments.SelectMany(range => Enumerable.Repeat(range.IdNumber, range.Length)).ToList();
+        // TestContext.Out.WriteLine(string.Concat(diskLayout.Select(x => x.ToString())).Replace("-1", "."));
+        long result = diskLayout.Select((value, index) => (long)value * index).Where(x => x > 0).Sum();
         
         Assert.That(result, Is.EqualTo(expectedResult));
     }
 
-    record Range(int startIndex, int length, int value);
+    static LinkedList<Range> ParseDiskSegments(string filename)
+    {
+        var input = File.ReadAllText(filename);
+        
+        if (input.Length % 2 != 0)
+        {
+            // add filler for chunk to work if last segment is missing
+            input += "0";
+        }
+        
+        var chunked = input.Chunk(2);
+        var diskContent = new LinkedList<Range>();
+        var blockID = 0;
+        foreach (var chunk in chunked)
+        {
+            diskContent.AddLast(new Range(int.Parse(chunk[0].ToString()), blockID));
+            blockID++;
+            diskContent.AddLast(new Range(int.Parse(chunk[1].ToString()), -1));
+        }
+
+        return diskContent;
+    }
+
+    static LinkedListNode<Range> RearrangeBlock(LinkedListNode<Range> currentBlock)
+    {
+        var diskContent = currentBlock.List;
+        var potentialBlock = diskContent.First;
+        while (potentialBlock != null && potentialBlock != currentBlock)
+        {
+            if (potentialBlock.Value.IdNumber == -1
+                && potentialBlock.Value.Length >= currentBlock.Value.Length)
+            {
+                // split existing free block into used and left-over blocks:
+                diskContent.AddBefore(potentialBlock, new Range(currentBlock.Value.Length, currentBlock.Value.IdNumber));
+                diskContent.AddBefore(potentialBlock, new Range(potentialBlock.Value.Length - currentBlock.Value.Length, -1));
+                diskContent.Remove(potentialBlock);
+                
+                // free original space of the moved block:
+                // Note: This approach doesn't combine multiple free segments and would make them unavailable for larger blocks
+                // due to the strict ordering, this isn't an issue as the freed-up spaces are only at the end and we never move blocks backwards
+                currentBlock.Value = currentBlock.Value with { IdNumber = -1 };
+                break;
+            }
+
+            potentialBlock = potentialBlock.Next;
+        }
+
+        return currentBlock;
+    }
+
+    record Range(int Length, int IdNumber);
 }
