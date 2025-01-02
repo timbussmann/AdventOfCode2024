422c535a4a43be8caccc751c461a8c6ed124e5ae
Tim Bussmann <timbussmann@users.noreply.github.com>
2025-01-02 21:39:15 +0100

    refactoring day 10

--- AdventOfCode2024/Day10.cs
+++ AdventOfCode2024/Day10.cs
@@ -6,32 +6,25 @@ public class Day10
     [TestCase("Day10.Input.txt", 496)]
     public void Part1(string filename, int expectedResult)
     {
-        var input = File.ReadAllLines(filename);
-        
-        Dictionary<(int, int), int> map = new();
-        for (var y = 0; y < input.Length; y++)
-        {
-            for (var x = 0; x < input[y].Length; x++)
-            {
-                map.Add((y, x), int.Parse(input[y][x].ToString()));
-            }
-        }
+        var map = ParseMap(filename);
+        var trails = GetAllTrails(map);
 
-        var trailEnds = map.Where(pair => pair.Value == 9).Select(pair => pair.Key);
-
-
-        Dictionary<(int, int), List<(int, int)>> trails = new();
-        foreach (var trailEnd in trailEnds)
-        {
-            GetNumberOfTrails(trailEnd, trailEnd, map, trails);
-        }
-
-        Assert.That(trails.Select(kvp => kvp.Value.Distinct().Count()).Sum(), Is.EqualTo(expectedResult));
+        var trailScores = trails.Select(kvp => kvp.Value.Distinct().Count());
+        Assert.That(trailScores.Sum(), Is.EqualTo(expectedResult));
     }
-    
+
     [TestCase("Day10.Test.txt", 81)]
     [TestCase("Day10.Input.txt", 1120)]
     public void Part2(string filename, int expectedResult)
+    {
+        var map = ParseMap(filename);
+        var trails = GetAllTrails(map);
+
+        var trailRatings = trails.Select(kvp => kvp.Value.Count);
+        Assert.That(trailRatings.Sum(), Is.EqualTo(expectedResult));
+    }
+
+    static Dictionary<(int, int), int> ParseMap(string filename)
     {
         var input = File.ReadAllLines(filename);
         
@@ -44,21 +37,23 @@ public class Day10
             }
         }
 
-        var trailEnds = map.Where(pair => pair.Value == 9).Select(pair => pair.Key);
+        return map;
+    }
 
+    Dictionary<(int trailHeadY, int trailHeadX), List<(int trailEndY, int trailEndX)>> GetAllTrails(Dictionary<(int, int), int> map)
+    {
+        var trailEnds = map.Where(pair => pair.Value == 9).Select(pair => pair.Key);
 
         Dictionary<(int, int), List<(int, int)>> trails = new();
         foreach (var trailEnd in trailEnds)
         {
-            GetNumberOfTrails(trailEnd, trailEnd, map, trails);
+            FindTrails(currentPosition: trailEnd, trailEnd: trailEnd, map, trails);
         }
 
-        Assert.That(trails.Select(kvp => kvp.Value.Count).Sum(), Is.EqualTo(expectedResult));
+        return trails;
     }
 
-    static readonly (int, int)[] Directions = [(1, 0), (0, -1), (-1, 0), (0, 1)];
-
-    void GetNumberOfTrails((int y, int x) currentPosition, (int y, int x) trailEnd, Dictionary<(int, int), int> mapDic, Dictionary<(int, int), List<(int, int)>> trails)
+    void FindTrails((int y, int x) currentPosition, (int y, int x) trailEnd, Dictionary<(int, int), int> mapDic, Dictionary<(int, int), List<(int, int)>> trails)
     {
         var expectedNextHeight = mapDic[currentPosition] - 1;
         foreach ((int dy, int dx) direction in Directions)
@@ -74,9 +69,11 @@ public class Day10
                 }
                 else
                 {
-                    GetNumberOfTrails(newPos, trailEnd, mapDic,trails);
+                    FindTrails(newPos, trailEnd, mapDic, trails);
                 }
             }
         }
     }
+
+    static readonly (int, int)[] Directions = [(1, 0), (0, -1), (-1, 0), (0, 1)];
 }
