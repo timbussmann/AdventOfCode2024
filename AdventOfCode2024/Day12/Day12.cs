ae71b16eb5643c5188a562988049f48310b4a3ab
Tim Bussmann <timbussmann@users.noreply.github.com>
2025-02-23 17:33:53 +0100

    day 12 - some refactoring

--- AdventOfCode2024/Day12/Day12.cs
+++ AdventOfCode2024/Day12/Day12.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace AdventOfCode2024.Day12;
 
 public class Day12
@@ -8,66 +6,54 @@ public class Day12
     [TestCase("Day12.Input.txt", 1371306)]
     public void Part1(string filename, int expectedResult)
     {
-        var grid = File.ReadAllLines(Input.GetFilePath(filename));
-        Dictionary<(int y, int x), Area> map = new();
-        for (int y = 0; y < grid.Length; y++)
+        Dictionary<(int y, int x), char> grid = GridMap(File.ReadAllLines(Input.GetFilePath(filename)), c => c);
+        Dictionary<(int y, int x), Area> areasMap = new();
+        foreach (var coords in grid)
         {
-            for (int x = 0; x < grid[0].Length; x++)
-            {
-                MapArea(y, x, map, grid, new Area { Type = grid[y][x] });
-            }
+            MapArea(coords.Key, coords.Value, areasMap, grid, new Area { Type = coords.Value });
         }
         
-        var result = map.Values.Distinct().Sum(area => area.Plots.Count * area.CalculatePerimeter(map));
+        var result = areasMap.Values.Distinct().Sum(area => area.Plots.Count * area.CalculatePerimeter(areasMap));
+        
         Assert.That(result, Is.EqualTo(expectedResult));
     }
-    
+
     [TestCase("Day12.Test.txt", 1206)]
     [TestCase("Day12.Part2Example1.txt", 236)]
     [TestCase("Day12.Part2Example2.txt", 368)]
     [TestCase("Day12.Input.txt", 805880)]
     public void Part2(string filename, int expectedResult)
     {
-        var grid = File.ReadAllLines(Input.GetFilePath(filename));
-        Dictionary<(int y, int x), Area> map = new();
-        for (int y = 0; y < grid.Length; y++)
+        Dictionary<(int y, int x), char> grid = GridMap(File.ReadAllLines(Input.GetFilePath(filename)), c => c);
+        Dictionary<(int y, int x), Area> areasMap = new();
+        foreach (var coords in grid)
         {
-            for (int x = 0; x < grid[0].Length; x++)
-            {
-                MapArea(y, x, map, grid, new Area { Type = grid[y][x] });
-            }
+            MapArea(coords.Key, coords.Value, areasMap, grid, new Area { Type = coords.Value });
         }
         
-        var result = map.Values.Distinct()
-            .Select(area =>
-            {
-                Console.WriteLine($"Area: {area.Type} - plots: {area.Plots.Count}, sides: {area.CalculateSides(map)}");
-                return area;
-            })
-            .Sum(area => area.Plots.Count * area.CalculateSides(map));
+        var result = areasMap.Values.Distinct().Sum(area => area.Plots.Count * area.CalculateSides(areasMap));
+        
         Assert.That(result, Is.EqualTo(expectedResult));
     }
 
-    void MapArea(int y, int x, Dictionary<(int y, int x), Area> map, string[] grid, Area currentArea)
+    void MapArea((int y, int x) coords, char coordsType, Dictionary<(int y, int x), Area> map, Dictionary<(int y, int x), char> grid, Area currentArea)
     {
-        if (y < 0 || y >= grid.Length || x < 0 || x >= grid[0].Length)
+        if (!map.TryGetValue(coords, out _) && coordsType == currentArea.Type)
         {
-            return;
-        }
-
-        if (!map.TryGetValue((y,x), out _) && grid[y][x] == currentArea.Type)
-        {
-            currentArea.Plots.Add((y, x));
-            map.Add((y, x), currentArea);
+            currentArea.Plots.Add(coords);
+            map.Add(coords, currentArea);
         }
         else
         {
             return;
         }
         
-        ForEachNeighbour((y, x), (n, _) =>
+        ForEachNeighbour(coords, (n, _) =>
         {
-            MapArea(n.y, n.x, map, grid, currentArea);
+            if (grid.TryGetValue(n, out char c))
+            {
+                MapArea(n, c, map, grid, currentArea);
+            }
         });
     }
 
@@ -110,7 +96,7 @@ public class Day12
         
         public int CalculateSides(Dictionary<(int y, int x), Area> map)
         {
-            List<(Direction direction, (int y, int x) plot)> x = new();
+            List<(Direction direction, (int y, int x) plot)> connectedArea = new();
             foreach (var plot in Plots)
             {
                 ForEachNeighbour(plot, (n, d) =>
@@ -118,51 +104,49 @@ public class Day12
                     var neighbourArea = map.GetValueOrDefault(n);
                     if (neighbourArea != this)
                     {
-                        x.Add((d, plot));
+                        connectedArea.Add((d, plot));
                     }
                 });
-                
             }
 
             var counter = 0;
-            var directions = x.GroupBy(tuple => tuple.direction);
-            foreach (var direction in directions)
+            foreach (var direction in connectedArea.GroupBy(tuple => tuple.direction))
             {
-                if (direction.Key == Direction.Up || direction.Key == Direction.Down)
-                {
-                    var sidesByY = direction.GroupBy(p => p.plot.y);
-                    foreach (var yLine in sidesByY)
-                    {
-                        var sides = yLine.OrderBy(p => p.plot.x).ToList();
-                        counter++;
-                        for (int i = 1; i < sides.Count; i++)
-                        {
-                            if (Math.Abs(sides[i].plot.x - sides[i-1].plot.x) > 1)
-                            {
-                                counter++;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    var sidesByX = direction.GroupBy(p => p.plot.x);
-                    foreach (var xLine in sidesByX)
-                    {
-                        var sides = xLine.OrderBy(p => p.plot.y).ToList();
-                        counter++;
-                        for (int i = 1; i < sides.Count; i++)
-                        {
-                            if (Math.Abs(sides[i].plot.y - sides[i-1].plot.y) > 1)
-                            {
-                                counter++;
-                            }
-                        }
-                    }
-                }
+                var horizontalNeighbors = direction
+                    .GroupBy(p => direction.Key is Direction.Up or Direction.Down ? p.plot.y : p.plot.x)
+                    .Select(group => group
+                        .Select(tuple => direction.Key is Direction.Up or Direction.Down ? tuple.plot.x : tuple.plot.y)
+                        .OrderBy(x => x)
+                        .ToList());
                 
+                foreach (var neighborsOnSameLine in horizontalNeighbors)
+                {
+                    counter++;
+                    for (int i = 1; i < neighborsOnSameLine.Count; i++)
+                    {
+                        if (Math.Abs(neighborsOnSameLine[i] - neighborsOnSameLine[i-1]) > 1)
+                        {
+                            // if neighbors are not connected they belong to a different side
+                            counter++;
+                        }
+                    }
+                }
             }
             return counter;
         }
+    }
+
+    static Dictionary<(int y, int x), T> GridMap<T>(string[] input, Func<char, T> mapper)
+    {
+        var dic = new Dictionary<(int y, int x), T>();
+        for (int y = 0; y < input.Length; y++)
+        {
+            for (int x = 0; x < input[0].Length; x++)
+            {
+                dic.Add((y, x), mapper(input[y][x]));
+            }
+        }
+
+        return dic;
     }
 }
