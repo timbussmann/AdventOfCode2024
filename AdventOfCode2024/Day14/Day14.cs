332d9653ce7bc28a8ba5d46b03c2e905df109f4a
Tim Bussmann <timbussmann@users.noreply.github.com>
2025-04-06 18:07:41 +0200

    Day 14 - Part2

--- AdventOfCode2024/Day14/Day14.cs
+++ AdventOfCode2024/Day14/Day14.cs
@@ -19,7 +19,8 @@ public partial class Day14
         var heightMiddleIndex = (height - 1) / 2;
         foreach (var robot in robots)
         {
-            if (robot.Position.x == widthMiddleIndex || robot.Position.y == heightMiddleIndex) continue;
+            if (robot.Position.x == widthMiddleIndex || robot.Position.y == heightMiddleIndex) 
+                continue;
 
             var quadrantX = robot.Position.x < widthMiddleIndex ? 0 : 1;
             var quadrantY = robot.Position.y < heightMiddleIndex ? 0 : 1;
@@ -28,6 +29,38 @@ public partial class Day14
 
         var safetyFactor = quadrants.SelectMany(q => q).Aggregate(1L, (i, l) => i * l);
         Assert.That(safetyFactor, Is.EqualTo(expectedResult));
+    }
+
+    [TestCase("Day14.Input.txt", 101, 103, 6000, 8000)]
+    public void Part2(string filename, int width, int height, int renderStartRange, int renderEndRange)
+    {
+        var robots = ParseRobots("Day14.Input.txt");
+        var outputFile = File.CreateText("Day14.Output.txt");
+
+        var grid = new char[height * width];
+        for (var i = 0; i < renderEndRange; i++)
+        {
+            Array.Fill(grid, ' ');
+            foreach (var robot in robots)
+            {
+                robot.Move(width, height);
+                grid[robot.Position.y * width + robot.Position.x] = 'X';
+            }
+
+            if (i >= renderStartRange && i <= renderEndRange)
+            {
+                outputFile.WriteLine($"---------------------- {i + 1} seconds -----------------------");
+                foreach (var line in grid.Chunk(width))
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+            
+        }
+        
+        outputFile.Close();
+        
+        // 7520 seconds is the right answer - found by manually inspecting the output ;)
     }
 
     private static List<Robot> ParseRobots(string filename) =>
